a257f35370a4c0562e5cbc6597a1b33c3a28d164
Harman uppal <huppal03@gmail.com>
2020-03-05 13:10:09 +0530

    create rest API in wordpress

---
+++ index.php
@@ -0,0 +1,17 @@
+<?php
+
+//wev~v1
+register_rest_route( 'wp_json', '/third_party_accept', array(
+'methods'  => 'POST',//Can be any GET , POST , REQUEST
+'callback' => 'some_function'
+) );
+		
+		
+function some_function(){
+	/*
+	Do some stuff
+	*/
+	
+}
+
+// post to URL : https://www.site_url.com/wp-json/wp_json/leadfix_insert_data
